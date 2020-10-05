04b704cc68d300f82d5a003e8835f530c15d4e2b
Данил Дмитриев <danildmitriev@MacBook-Pro-Danil.local>
2020-10-05 15:28:52 +0300

    '_'

--- utilsSeq2seq.py
+++ utilsSeq2seq.py
@@ -42,7 +42,6 @@ def do_epoch(model, criterion, data_iter, optimizer=None, name=None):
                 name, epoch_loss / batches_count, math.exp(epoch_loss / batches_count))
             )
             progress_bar.refresh()
-
     return epoch_loss / batches_count
 
 
